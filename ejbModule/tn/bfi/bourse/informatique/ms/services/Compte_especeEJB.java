379f28f17cf679194026003cec49cce871872d98
Marwen <marwenhammami@live.fr>
2015-04-20 18:51:23 +0100

    c bon

--- ejbModule/tn/bfi/bourse/informatique/ms/services/Compte_especeEJB.java
+++ ejbModule/tn/bfi/bourse/informatique/ms/services/Compte_especeEJB.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
